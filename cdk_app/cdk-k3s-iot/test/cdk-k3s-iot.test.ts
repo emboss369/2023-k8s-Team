cabdb7e729679a0ce310a16f521c7191b46cecd1
emboss369 <emboss>
2023-11-29 22:50:55 +0900

    add cdk templete

---
+++ cdk_app/cdk-k3s-iot/test/cdk-k3s-iot.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkK3SIot from '../lib/cdk-k3s-iot-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-k3s-iot-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkK3SIot.CdkK3SIotStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
